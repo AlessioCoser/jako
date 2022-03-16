cb96e70b4be981da4918cb918c77b526a5983795
Alessio Coser <>
2022-03-16 18:13:21 +0100

    add comment to maximum pool size

--- src/main/kotlin/dbhelper/SessionManager.kt
+++ src/main/kotlin/dbhelper/SessionManager.kt
@@ -9,6 +9,7 @@ class SessionManager(jdbc: String, user: String, password: String) {
         dataSource.jdbcUrl = jdbc
         dataSource.username = user
         dataSource.password = password
+        // dataSource.maximumPoolSize // start with this: ((2 * core_count) + number_of_disks)
     }
 
     fun <T> session(fn: Session.() -> T): T {
