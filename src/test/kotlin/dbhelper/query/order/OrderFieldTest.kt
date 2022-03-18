78db3b1c119b71a61909cba13d7cf345e75515ab
Alessio Coser <>
2022-03-18 21:22:26 +0100

    orderField tests

---
+++ src/test/kotlin/dbhelper/query/order/OrderFieldTest.kt
@@ -0,0 +1,34 @@
+package dbhelper.query.order
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class OrderFieldTest {
+    @Test
+    fun `asc one field`() {
+        val asc = Asc("field1")
+
+        assertEquals("\"field1\" ASC", asc.toString())
+    }
+
+    @Test
+    fun `asc multiple fields`() {
+        val asc = Asc("field1", "field2", "field3")
+
+        assertEquals("\"field1\" ASC, \"field2\" ASC, \"field3\" ASC", asc.toString())
+    }
+
+    @Test
+    fun `desc one field`() {
+        val desc = Desc("field1")
+
+        assertEquals("\"field1\" DESC", desc.toString())
+    }
+
+    @Test
+    fun `desc multiple fields`() {
+        val desc = Desc("field1", "field2", "field3")
+
+        assertEquals("\"field1\" DESC, \"field2\" DESC, \"field3\" DESC", desc.toString())
+    }
+}
